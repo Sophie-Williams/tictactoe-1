63f1cea1c53305ea106be911d3b028841f9f8fc9
Oleksandr Yefremov <oleksandr.yefremov@gmail.com>
2018-06-10 00:03:27 +0300

    midnight

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <numeric>
 #include <string>
 #include <vector>
+#include <cmath>
+#include <assert.h>
 
 using namespace std;
 
@@ -58,7 +60,10 @@ const int win_rows[8][3] = {
 struct Move {
 	char board;
 	char pos;
+	bool operator==(Move m)const { return board == m.board&&pos == m.pos; }
 };
+
+void swap(char &side) { side ^= O ^ X; }
 
 short make_mini_board(short base, char pos, char side) {
 	auto pow = power3[pos];
@@ -109,9 +114,11 @@ struct Board {
 
 	unsigned int moves_count() const {
 		if (availableMiniBoard == ANY_MINIBOARD)
+		{
 			return accumulate(boards.begin(), boards.end(), 0, [](int sum, short b) {
-			return sum + miniBoards[b].moves_count;
-		});
+				return sum + miniBoards[b].moves_count;
+			});
+		}
 		return miniBoards[boards[availableMiniBoard]].moves_count;
 	}
 
@@ -131,6 +138,13 @@ struct Board {
 			for (int i = 0; i < super.moves_count; ++i)
 				add_moves(super.moves[i]);
 		}
+	}
+
+	vector<Move> legal_moves() const
+	{
+		vector<Move> moves;
+		legal_moves(moves);
+		return moves;
 	}
 
 	Move random_move(int rand) const {
@@ -153,24 +167,27 @@ struct Board {
 		}
 		Move m = { board, miniBoards[boards[board]].moves[rand] };
 		auto new_id = make_mini_board(boards[m.board], m.pos, O);
-		if (new_id > 20000)
-		{
-			int f = 5;
-		}
+
 		return{ board, miniBoards[boards[board]].moves[rand] };
 	}
 
-	char apply_move(Move m, char side) {
+	char apply_move(Move m) {
+		char side = nextMoveSide;
+		swap(nextMoveSide);
 		auto new_id = make_mini_board(boards[m.board], m.pos, side);
 		boards[m.board] = new_id;
 
-		auto b = miniBoards[new_id];
+		const auto& b = miniBoards[new_id];
 		if (b.side == side)
 		{
 			superBoard = make_mini_board(superBoard, m.board, side);
 
-			auto& super = miniBoards[superBoard];
-			if (super.side == side) return side;
+			const auto& super = miniBoards[superBoard];
+			if (super.side == side)
+			{
+				availableMiniBoard = side;
+				return side;
+			}
 		}
 		if (b.moves_count == 0)
 		{
@@ -181,8 +198,17 @@ struct Board {
 			if (!has_any_move)
 			{
 				auto& super = miniBoards[superBoard];
-				if (super.num_x > super.num_o) return X;
-				if (super.num_x < super.num_o) return O;
+				if (super.num_x > super.num_o)
+				{
+					availableMiniBoard = X;
+					return X;
+				}
+				if (super.num_x < super.num_o)
+				{
+					availableMiniBoard = O;
+					return O;
+				}
+				availableMiniBoard = DRAW;
 				return DRAW;
 			}
 		}
@@ -198,23 +224,48 @@ struct Board {
 	}
 };
 
-void swap(char &side) { side ^= O ^ X; }
+struct GameStat
+{
+	int X_wins = 0, O_wins = 0;
+};
 
-int single_playout(Board b, char side) {
+struct Node
+{
+	Node* parent = nullptr;
+	Node* first = nullptr;
+	GameStat stat;
+	Board board;
+	short children_size = 0;
+	Move move;
+};
+
+vector<Node> nodes_pool(10 * 1000 * 1000);
+Node* nodes_pool_head = &nodes_pool.front();
+
+Node* allocate_nodes(short count)
+{
+	auto res = nodes_pool_head;
+	nodes_pool_head += count;
+	assert(nodes_pool_head <= &nodes_pool.back());
+	return res;
+}
+
+int single_playout(Board b) {
 	auto res = IN_PROGRESS;
 	while (res == IN_PROGRESS) {
 		int r = rand();
-		res = b.apply_move(b.random_move(r), side);
-		swap(side);
+		res = b.apply_move(b.random_move(r));
 	}
 	return res;
 }
 
-int playout(const Board& b, int n, char side)
+GameStat playout(const Board& b, int n)
 {
-	int res = 0;
+	GameStat res;
 	while (n-- > 0)
-		res += single_playout(b, side) == side;
+		if (single_playout(b) == X)
+			res.X_wins += 1;
+		else res.O_wins += 1;
 	return res;
 }
 
@@ -262,7 +313,7 @@ Move chooseMove(const Board& b)
 	for (auto m : legal_moves)
 	{
 		auto board_copy = b;
-		auto res = board_copy.apply_move(m, X);
+		auto res = board_copy.apply_move(m);
 		int score = 0;
 		const int N = 100;
 		if (res == X)
@@ -271,7 +322,12 @@ Move chooseMove(const Board& b)
 			score = 0;
 		else if (res == DRAW)
 			score = N / 2;
-		else score = N - playout(board_copy, N, O);
+		else
+		{
+			auto stat = playout(board_copy, N);
+			if (b.nextMoveSide == X) score = stat.X_wins;
+			else score = stat.O_wins;
+		}
 
 		cerr << (int)m.board << "-" << (int)m.pos << " score " << score << endl;
 
@@ -307,34 +363,254 @@ void print(const Board& b)
 	}
 }
 
+Node* make_root_node(const Board& b)
+{
+	auto node = allocate_nodes(1);
+	node->board = b;
+	return node;
+}
+
+bool has_moves(const Board& b)
+{
+	return b.availableMiniBoard <= ANY_MINIBOARD;
+}
+
+double childPotential(const Node* child, char side, double log_t)
+{
+	auto& s = child->stat;
+	double n = s.X_wins + s.O_wins;
+	double w = side == X ? s.X_wins : s.O_wins;
+
+	assert(n > 0);
+	return w / n + 1.4 * sqrt(log_t / n);
+}
+
+double childValue(const Node* child, char side)
+{
+	auto& s = child->stat;
+	double n = s.X_wins + s.O_wins;
+	double w = side == X ? s.X_wins : s.O_wins;
+
+	assert(n > 0);
+	return w / n;
+}
+
+Node* find_leaf_node(Node* root)
+{
+	while (root->children_size > 0)
+	{
+		auto& s = root->stat;
+		double t = s.X_wins + s.O_wins;
+		assert(t > 0);
+		const auto log_t = log(t);
+		auto side = root->board.nextMoveSide;
+
+		double best = -999;
+		Node* best_node = nullptr;
+		for (int i = 0; i < root->children_size; ++i)
+		{
+			auto node = root->first + i;
+			auto val = childPotential(node, side, log_t);
+			if (val > best || !best_node)
+			{
+				best = val;
+				best_node = node;
+			}
+		}
+
+		root = best_node;
+	}
+
+	return root;
+}
+
+
+Node* chose_best_node(Node* root)
+{
+	auto side = root->board.nextMoveSide;
+
+	double best = -999;
+	Node* best_node = nullptr;
+	for (int i = 0; i < root->children_size; ++i)
+	{
+		auto node = root->first + i;
+		auto val = childValue(node, side);
+		if (val > best || !best_node)
+		{
+			best = val;
+			best_node = node;
+		}
+	}
+
+	return best_node;
+}
+
+void playout_leaf(Node* node, int n)
+{
+	if (!has_moves(node->board))
+	{
+		const int BIG_NUM = 100;
+		if (node->board.availableMiniBoard == X)
+		{
+			node->stat.X_wins += BIG_NUM * n;
+		}
+		else
+		{
+			// count draw as a win for O
+			node->stat.O_wins += BIG_NUM * n;
+		}
+	}
+	else
+	{
+		node->stat = playout(node->board, n);
+	}
+}
+
+void backpropagate(Node* node, GameStat stat)
+{
+	while (node)
+	{
+		node->stat.X_wins += stat.X_wins;
+		node->stat.O_wins += stat.O_wins;
+		node = node->parent;
+	}
+}
+
+GameStat generate_and_playout_children_for_empty_board(Node* node, int playouts)
+{
+	GameStat result;
+
+	vector<Move> moves = { 
+		{ 0, 0 }, { 0, 1 }, { 0, 4 },
+		{ 1, 0 }, { 1, 1 }, { 1, 4 },
+		{ 4, 0 }, { 4, 1 }, { 4, 4 } };
+	auto next_node = allocate_nodes(moves.size());
+	node->first = next_node;
+	node->children_size = moves.size();
+	const auto side = node->board.nextMoveSide;
+	for (auto m : moves)
+	{
+		auto n = next_node++;
+
+		n->parent = node;
+		n->move = m;
+		n->board = node->board;
+		auto& b = n->board;
+		b.apply_move(m);
+		playout_leaf(n, playouts);
+		result.X_wins += n->stat.X_wins;
+		result.O_wins += n->stat.O_wins;
+	}
+	return result;
+}
+
+GameStat generate_and_playout_children(Node* node, int playouts)
+{
+	GameStat result;
+
+	auto moves = node->board.legal_moves();
+	auto next_node = allocate_nodes(moves.size());
+	node->first = next_node;
+	node->children_size = moves.size();
+	const auto side = node->board.nextMoveSide;
+	for (auto m : moves)
+	{
+		auto n = next_node++;
+
+		n->parent = node;
+		n->move = m;
+		n->board = node->board;
+		auto& b = n->board;
+		b.apply_move(m);
+		playout_leaf(n, playouts);
+		result.X_wins += n->stat.X_wins;
+		result.O_wins += n->stat.O_wins;
+	}
+	return result;
+}
+
+void travel_tree_and_playout(Node* root)
+{
+	auto leaf = find_leaf_node(root);
+	auto stat = generate_and_playout_children(leaf, 10);
+	backpropagate(leaf, stat);
+}
+
+void playout_empty_board(Node* root)
+{
+	auto stat = generate_and_playout_children_for_empty_board(root, 10);
+	backpropagate(root, stat);
+}
+
+Node* find_node_by_move(Node* root, Move m)
+{
+	if (root->children_size == 0)
+	{
+		generate_and_playout_children(root, 1);
+	}
+	for (int i = 0; i < root->children_size; ++i)
+	{
+		auto node = root->first + i;
+		if (node->move == m)
+			return node;
+	}
+
+	auto board = root->board;
+	board.apply_move(m);
+	root = make_root_node(board);
+
+	cerr << "Start withfresh node" << endl;
+
+	return root;
+}
+
+
 int main() {
 	vector<vector<int>> fields(10, vector<int>(10, 0));
 
-	Board b;
-
-	initialBoard(b);
+	Node* root = nullptr;
 
 	// game loop
 	while (1) {
 		int opponentRow;
 		int opponentCol;
 		cin >> opponentRow >> opponentCol;
-
-		if (opponentCol != -1 && opponentRow != -1) {
-			auto opponentMove = make_move(opponentRow, opponentCol);
-			b.apply_move(opponentMove, O);
-		}
-
 		skip_valid_actions();
 
-		Move my_move = chooseMove(b);
+		if (root)
+		{
+			auto opponentMove = make_move(opponentRow, opponentCol);
+			root = find_node_by_move(root, opponentMove);
+			root->parent = nullptr;
+		}
+		else
+		{
+			Board b;
+			initialBoard(b);
 
-		print(b);
-		cerr << "move to " << (int)my_move.board << "-" << (int)my_move.pos << endl;
-		b.apply_move(my_move, X);
-		print(b);
+			if (opponentCol != -1 && opponentRow != -1) {
+				auto opponentMove = make_move(opponentRow, opponentCol);
+				b.apply_move(opponentMove);
+			}
 
-		auto res = get_row_and_col(my_move);
+			root = make_root_node(b);
+
+			if (opponentCol != -1 && opponentRow != -1) {
+				playout_empty_board(root);
+			}
+		}
+
+		cerr << "node has " << (root->stat.O_wins + root->stat.X_wins) << " plays\n";
+
+		for (int i = 0; i < 10; ++i)
+		{
+			travel_tree_and_playout(root);
+		}
+
+		root = chose_best_node(root);
+		root->parent = nullptr;
+
+		auto res = get_row_and_col(root->move);
 		cout << res.first << " " << res.second << endl;
 	}
 }
